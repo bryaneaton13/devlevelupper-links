bac83b4e286e4f3454a5391526ba63c08daa842d
Bryan Eaton <bryaneaton13@gmail.com>
2024-08-27 23:16:24 -0400

    logo

--- components/TitleCard.tsx
+++ components/TitleCard.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { MainChannelName } from '@/lib/config';
+import Image from "next/image";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { MainChannelName } from "@/lib/config";
 
 export default function TitleCard() {
   return (
@@ -9,9 +9,9 @@ export default function TitleCard() {
         <CardTitle>
           <p className="pb-5 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{MainChannelName}</p>
         </CardTitle>
-        {/* <CardDescription className="flex justify-center pb-2">
-          <Image src="/squib_coin_sticker.png" alt="haha" width={150} height={150} />
-        </CardDescription> */}
+        <CardDescription className="flex justify-center pb-2">
+          <Image src="/assets/dev_highlights.png" alt="dev" width={150} height={150} />
+        </CardDescription>
       </CardHeader>
     </Card>
   );
